c6e9022752ac95b291538bb8b359b3f6dfad36d6
Sigve <sigvetl@student.matnat.uio.no>
2020-08-31 16:24:53 +0200

    lagt til valgfrie oppgaver

--- assignment2/move.sh
+++ assignment2/move.sh
@@ -13,20 +13,47 @@ function move {
     echo "gyldig dir"
   elif [ ! -d $src ] && [ ! -d $dst ]; then
     echo "ugyldige dir"
+    exit 1
   elif [ ! -d $src ]; then
     echo "ugyldig src"
+    exit 1
   elif [ ! -d $dst ]; then
-    echo "ugyldig dst"
+    echo "ugyldig dst. Vil du opprette dst?\ny/n"
+    read input
+    while [ $input != y ] && [ $input != n ]; do
+      echo "ugyldig dst. Vil du opprette dst?\ny/n"
+      read input
+    done
+    if [ $input = y ]; then
+      mkdir $dst
+    else
+      exit 1
+    fi
   fi
 
-  files=`ls $src`
+  cd $src
+
+  echo "Do you want to move all files or .txt files?\nall/txt"
+  read input
+
+  while [ $input != txt ] && [ $input != all ]; do
+    echo "Do you want to move all files or .txt files?\nall/txt"
+    read input
+  done
+
+  if [ $input == txt ]; then
+    files=`ls *.txt`
+  elif [ $input == all ]; then
+    files=`ls`
+  fi
+
   echo $files
 
   dato=$(date +"%F"-"%k"-"%M")
   mkdir $dst/$dato
 
   for file in $files; do
-    mv $src/$file $dst/$dato
+    mv $file $dst/$dato
   done
 
 }
