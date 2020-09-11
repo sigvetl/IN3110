ef95ff4dfa1b3ede47a9c45324a0e86705b2a1a2
Sigve <sigvetl@student.matnat.uio.no>
2020-09-11 16:00:51 +0200

    added solution to 3.4 and 3.5

--- assignment3/Array.py
+++ assignment3/Array.py
@@ -5,19 +5,40 @@ class Array:
 
     def __init__(self, shape, *values):
         self.array = []
-        self.shape = shape[0]
         number = (int, float)
-        if isinstance(values[0], number):
-            type = number
-        elif isinstance(values[0], bool):
-            type = bool
-        if self.shape != len(values):
+        if len(shape) == 2:
+            counter = 0
+            #two dimensional
+            self.shape = shape
+            if isinstance(values[0], number):
+                type = number
+            elif isinstance(values[0], bool):
+                type = bool
+            if (self.shape[0] * self.shape[1]) != len(values):
                 raise ValueError("Shape is not equal to number of values")
-        for i in values:
-            if not isinstance(i, type):
-                raise ValueError("Values are not of same type")
-            else:
-                self.array.append(i)
+            for i in range(self.shape[0]):
+                new = []
+                for j in range(self.shape[1]):
+                    if not isinstance(values[counter], type):
+                        raise ValueError("Values are not of same type")
+                    else:
+                        new.append(values[counter])
+                        counter+=1
+                self.array.append(new)
+
+        elif len(shape) == 1:
+            self.shape = shape
+            if isinstance(values[0], number):
+                type = number
+            elif isinstance(values[0], bool):
+                type = bool
+            if self.shape[0] != len(values):
+                raise ValueError("Shape is not equal to number of values")
+            for i in values:
+                if not isinstance(i, type):
+                    raise ValueError("Values are not of same type")
+                else:
+                    self.array.append(i)
 
         """
         Make sure that you check that your array actually is an array, which means it is homogeneous (one data type).
@@ -31,10 +52,19 @@ class Array:
 
     def __str__(self):
         stringrep = "["
-        for i in range(len(self.array)):
-            stringrep+= str(self.array[i])
-            if (i != len(self.array)-1):
-                stringrep+= ", "
+        if len(self.shape) == 2:
+            for i in range(len(self.array)):
+                stringrep += "["
+                for j in range(self.shape[1]):
+                    stringrep += str(self.array[i][j])
+                    if (j != self.shape[1] -1):
+                        stringrep += ", "
+                stringrep += "]"
+        else:
+            for i in range(len(self.array)):
+                stringrep+= str(self.array[i])
+                if (i != len(self.array)-1):
+                    stringrep+= ", "
         stringrep+= "]"
         return stringrep
 
@@ -46,18 +76,25 @@ class Array:
     def __add__(self, other):
         newArray = []
         if isinstance(other, (int, float)):
-            for i in self.array:
-                newArray.append(i + other)
+            for i in range(len(self.array)):
+                if len(self.shape) == 2:
+                    for j in range(self.shape[1]):
+                        newArray.append(self.array[i][j] + other)
+                else:
+                    newArray.append(self.array[i] + other)
         elif isinstance(other, Array):
             if self.shape == other.shape:
                 for i in range(len(self.array)):
-                    newArray.append(self.array[i] + other.array[i])
+                    if len(self.shape) == 2:
+                        for j in range(self.shape[1]):
+                            newArray.append(self.array[i][j] + other.array[i][j])
+                    else:
+                        newArray.append(self.array[i] + other.array[i])
             else:
-                return "NotImplemented"
+                return NotImplemented
         else:
-            return "NotImplemented"
-        shapeTuple = (self.shape,)
-        returnArray = Array(shapeTuple, *newArray)
+            return NotImplemented
+        returnArray = Array(self.shape, *newArray)
         return returnArray
         """Element-wise adds Array with another Array or number.
         If the method does not support the operation with the supplied arguments
@@ -69,6 +106,28 @@ class Array:
         """
 
     def __radd__(self, other):
+        newArray = []
+        if isinstance(other, (int, float)):
+            for i in range(len(self.array)):
+                if len(self.shape) == 2:
+                    for j in range(self.shape[1]):
+                        newArray.append(self.array[i][j] + other)
+                else:
+                    newArray.append(self.array[i] + other)
+        elif isinstance(other, Array):
+            if self.shape == other.shape:
+                for i in range(len(self.array)):
+                    if len(self.shape) == 2:
+                        for j in range(self.shape[1]):
+                            newArray.append(self.array[i][j] + other.array[i][j])
+                    else:
+                        newArray.append(self.array[i] + other.array[i])
+            else:
+                return NotImplemented
+        else:
+            return NotImplemented
+        returnArray = Array(self.shape, *newArray)
+        return returnArray
         """Element-wise adds Array with another Array or number.
         If the method does not support the operation with the supplied arguments
         (specific data type or shape), it should return NotImplemented.
@@ -82,18 +141,25 @@ class Array:
     def __sub__(self, other):
         newArray = []
         if isinstance(other, (int, float)):
-            for i in self.array:
-                newArray.append(i - other)
+            for i in range(len(self.array)):
+                if len(self.shape) == 2:
+                    for j in range(self.shape[1]):
+                        newArray.append(self.array[i][j] - other)
+                else:
+                    newArray.append(self.array[i] - other)
         elif isinstance(other, Array):
             if self.shape == other.shape:
                 for i in range(len(self.array)):
-                    newArray.append(self.array[i] - other.array[i])
+                    if len(self.shape) == 2:
+                        for j in range(self.shape[1]):
+                            newArray.append(self.array[i][j] - other.array[i][j])
+                    else:
+                        newArray.append(self.array[i] - other.array[i])
             else:
-                return "NotImplemented"
+                return NotImplemented
         else:
-            return "NotImplemented"
-        shapeTuple = (self.shape,)
-        returnArray = Array(shapeTuple, *newArray)
+            return NotImplemented
+        returnArray = Array(self.shape, *newArray)
         return returnArray
         """Element-wise subtracts an Array or number from this Array.
         If the method does not support the operation with the supplied arguments
@@ -106,6 +172,28 @@ class Array:
         pass
 
     def __rsub__(self, other):
+        newArray = []
+        if isinstance(other, (int, float)):
+            for i in range(len(self.array)):
+                if len(self.shape) == 2:
+                    for j in range(self.shape[1]):
+                        newArray.append(self.array[i][j] - other)
+                else:
+                    newArray.append(self.array[i] - other)
+        elif isinstance(other, Array):
+            if self.shape == other.shape:
+                for i in range(len(self.array)):
+                    if len(self.shape) == 2:
+                        for j in range(self.shape[1]):
+                            newArray.append(self.array[i][j] - other.array[i][j])
+                    else:
+                        newArray.append(self.array[i] - other.array[i])
+            else:
+                return NotImplemented
+        else:
+            return NotImplemented
+        returnArray = Array(self.shape, *newArray)
+        return returnArray
         """Element-wise subtracts this Array from a number or Array.
         If the method does not support the operation with the supplied arguments
         (specific data type or shape), it should return NotImplemented.
@@ -119,18 +207,25 @@ class Array:
     def __mul__(self, other):
         newArray = []
         if isinstance(other, (int, float)):
-            for i in self.array:
-                newArray.append(i * other)
+            for i in range(len(self.array)):
+                if len(self.shape) == 2:
+                    for j in range(self.shape[1]):
+                        newArray.append(self.array[i][j] * other)
+                else:
+                    newArray.append(self.array[i] * other)
         elif isinstance(other, Array):
             if self.shape == other.shape:
                 for i in range(len(self.array)):
-                    newArray.append(self.array[i] * other.array[i])
+                    if len(self.shape) == 2:
+                        for j in range(self.shape[1]):
+                            newArray.append(self.array[i][j] * other.array[i][j])
+                    else:
+                        newArray.append(self.array[i] * other.array[i])
             else:
-                return "NotImplemented"
+                return NotImplemented
         else:
-            return "NotImplemented"
-        shapeTuple = (self.shape,)
-        returnArray = Array(shapeTuple, *newArray)
+            return NotImplemented
+        returnArray = Array(self.shape, *newArray)
         return returnArray
         """Element-wise multiplies this Array with a number or array.
         If the method does not support the operation with the supplied arguments
@@ -142,6 +237,28 @@ class Array:
         """
 
     def __rmul__(self, other):
+        newArray = []
+        if isinstance(other, (int, float)):
+            for i in range(len(self.array)):
+                if len(self.shape) == 2:
+                    for j in range(self.shape[1]):
+                        newArray.append(self.array[i][j] * other)
+                else:
+                    newArray.append(self.array[i] * other)
+        elif isinstance(other, Array):
+            if self.shape == other.shape:
+                for i in range(len(self.array)):
+                    if len(self.shape) == 2:
+                        for j in range(self.shape[1]):
+                            newArray.append(self.array[i][j] * other.array[i][j])
+                    else:
+                        newArray.append(self.array[i] * other.array[i])
+            else:
+                return NotImplemented
+        else:
+            return NotImplemented
+        returnArray = Array(self.shape, *newArray)
+        return returnArray
         """Element-wise multiplies this Array with a number or array.
         If the method does not support the operation with the supplied arguments
         (specific data type or shape), it should return NotImplemented.
@@ -155,9 +272,14 @@ class Array:
     def __eq__(self, other):
         if len(self.array) == len(other.array):
             for i in range(len(self.array)):
-                if self.array[i] != other.array[i]:
-                    return False
-                return True
+                if len(self.shape) == 2:
+                    for j in range(self.shape[1]):
+                        if self.array[i][j] != other.array[i][j]:
+                            return False
+                else:
+                    if self.array[i] != other.array[i]:
+                        return False
+            return True
         else:
             return False
         """Compares an Array with another Array.
@@ -168,26 +290,39 @@ class Array:
         Returns:
             bool: True if the two arrays are equal. False otherwise.
         """
-
+    #mangler
     def is_equal(self, other):
         newArray = []
         if isinstance(other, (int, float)):
-            for i in self.array:
-                if i != other:
-                    newArray.append(False)
+            for i in range(len(self.array)):
+                if len(self.shape) == 2:
+                    for j in range(self.shape[1]):
+                        if self.array[i][j] != other:
+                            newArray.append(False)
+                        else:
+                            newArray.append(True)
                 else:
-                    newArray.append(True)
-        elif isinstance(other, Array):
-            if len(self.array) == len(other.array):
-                for i in range(len(self.array)):
-                    if self.array[i] != other.array[i]:
+                    if self.array[i] != other:
                         newArray.append(False)
                     else:
                         newArray.append(True)
+        elif isinstance(other, Array):
+            if len(self.array) == len(other.array):
+                for i in range(len(self.array)):
+                    if len(self.shape) == 2:
+                        for j in range(self.shape[1]):
+                            if self.array[i][j] != other.array[i][j]:
+                                newArray.append(False)
+                            else:
+                                newArray.append(True)
+                    else:
+                        if self.array[i] != other.array[i]:
+                            newArray.append(False)
+                        else:
+                            newArray.append(True)
             else:
                 return False
-        shapeTuple = (self.shape,)
-        returnArray = Array(shapeTuple, *newArray)
+        returnArray = Array(self.shape, *newArray)
         return returnArray
         """Compares an Array element-wise with another Array or number.
         If `other` is an array and the two array shapes do not match, this method should raise ValueError.
@@ -203,9 +338,17 @@ class Array:
 
     def mean(self):
         total = 0
-        for i in self.array:
-            total += i
-        return total/len(self.array)
+        lengde = 1
+        for i in self.shape:
+            lengde *= i
+        for i in range(len(self.array)):
+            if len(self.shape) == 2:
+                for j in range(self.shape[1]):
+                    total += self.array[i][j]
+            else:
+                total += self.array[i]
+
+        return total/lengde
         """Computes the mean of the array
         Only needs to work for numeric data types.
         Returns:
@@ -213,11 +356,18 @@ class Array:
         """
 
     def variance(self):
+        lengde = 1
+        for i in self.shape:
+            lengde *= i
         variance = 0
         mean = self.mean()
-        for i in self.array:
-            variance += ((i - mean)**2)
-        return variance/len(self.array)
+        for i in range(len(self.array)):
+            if len(self.shape) == 2:
+                for j in range(self.shape[1]):
+                    variance += ((self.array[i][j] - mean)**2)
+            else:
+                variance += ((self.array[i] - mean)**2)
+        return variance/lengde
         """Computes the variance of the array
         Only needs to work for numeric data types.
         The variance is computed as: mean((x - x.mean())**2)
@@ -226,10 +376,17 @@ class Array:
         """
 
     def min_element(self):
-        min = self.array[0]
-        for i in self.array:
-            if i < min:
-                min = i
+        if len(self.shape) == 2:
+            min = self.array[0][0]
+            for i in range(len(self.array)):
+                for j in range(self.shape[1]):
+                    if self.array[i][j] < min:
+                        min = self.array[i][j]
+        else:
+            min = self.array[0]
+            for i in self.array:
+                if i < min:
+                    min = i
         return min
         """Returns the smallest value of the array.
         Only needs to work for numeric data types.
--- assignment3/test_Array.py
+++ assignment3/test_Array.py
@@ -1,94 +1,114 @@
 #!/usr/bin/env python3
 
 from Array import Array
-
-#print-test
-def test_print(array, array2):
-    assert array.__str__() == "[1, 2, 3, 4]"
-    assert array2.__str__() == "[2, 4, 6, 8]"
-
-#tests for adding element to array
-def test_add_element(array, other, number):
-    assert array.__add__(other) == Array((4,), 6, 7, 8, 9)
-    assert array.__add__(number) == Array((4,), 4, 5, 6, 7)
-
-#tests for adding array to array
-def test_add_array(array, other, fail):
-    assert array.__add__(other) == Array((4,), 3, 6, 9, 12)
-    assert array.__add__(fail) == "NotImplemented"
-
-def test_add_element_2d(array, other):
-    #assert
-    pass
-
-#tests for subtracting element from array
-def test_sub_element(array, other, number):
-    assert array.__sub__(otherElement) == Array((4,), -4, -3, -2, -1)
-    assert array.__sub__(number) == Array((4,), -2, -1, 0, 1)
-
-#tests for subtracting array from array
-def test_sub_array(array, other, fail):
-    assert array.__sub__(other) == Array((4,), -1, -2, -3, -4)
-    assert array.__sub__(fail) == "NotImplemented"
-
-#tests for multiplying element with array
-def test_mul_element(array, other, number):
-    assert array.__mul__(other) == Array((4,), 5, 10, 15, 20)
-    assert array.__mul__(number) == Array((4,), 3, 6, 9, 12)
-
-#tests for multiplying array with array
-def test_mul_array(array, other, fail):
-    assert array.__mul__(other) == Array((4,), 2, 8, 18, 32)
-    assert array.__mul__(fail) == "NotImplemented"
-
-#test for checking if array is identical
-def test_eq(array1, array2, array3):
-    assert array1.__eq__(array2) == False
-    assert array1.__eq__(array3) == False
-    assert array1.__eq__(array1) == True
-
-#test for checking which elements are equal to a position in an array
-def test_is_equal(array1, array2, element):
-    assert array1.is_equal(array2) == Array((4,), False, False, False, False)
-    assert array1.is_equal(element) == Array((4,), False, False, True, False)
-
-#test for mean of an array
-def test_mean(array1, array2):
-    assert array1.mean() == 2.5
-    assert array2.mean() == 5
-
-#tests for variance of an array
-def test_variance(array1, array2):
-    assert array1.variance() == 1.25
-    assert array2.variance() == 5
-
-#tests for min_element of an array
-def test_min_element(array1, array2, array3):
-    assert array1.min_element() == 1
-    assert array2.min_element() != 5
-    assert array3.min_element() == 3
-
+import pytest
 
 shape = (4,)
 shape2 = (5,)
 shape2d = (2, 3)
-#test2dArray = Array(shape, 3, 3, 5, 9, 11, 1)
 testArray = Array(shape, 1, 2, 3, 4)
 testArray2 = Array(shape, 2, 4, 6, 8)
 failarray = Array(shape2, 4, 4, 5, 3, 9)
-otherElement = 5
-number = 3
-otherArray = [5, 6, 7, 8]
-#print(testArray)
-test_print(testArray, testArray2)
-test_add_element(testArray, otherElement, number)
-test_add_array(testArray, testArray2, failarray)
-test_sub_element(testArray, otherElement, number)
-test_sub_array(testArray, testArray2, failarray)
-test_mul_element(testArray, otherElement, number)
-test_mul_array(testArray, testArray2, failarray)
-test_eq(testArray, testArray2, failarray)
-test_is_equal(testArray, testArray2, number)
-test_mean(testArray, testArray2)
-test_variance(testArray, testArray2)
-test_min_element(testArray, testArray2, failarray)
+test2d = Array(shape2d, 3, 5, 1, 5, 6, 6)
+test2d2 = Array(shape2d, 3, 5, -10, 5, 0, 5)
+
+
+
+#print-test
+def test_print():
+    assert testArray.__str__() == "[1, 2, 3, 4]"
+    assert testArray2.__str__() == "[2, 4, 6, 8]"
+
+def test_print_2d():
+    assert test2d.__str__() == "[[3, 5, 1][5, 6, 6]]"
+    assert test2d2.__str__() == "[[3, 5, -10][5, 0, 5]]"
+
+#tests for adding element to array
+def test_add_element():
+    assert testArray.__add__(5) == Array((4,), 6, 7, 8, 9)
+    assert testArray.__add__(3) == Array((4,), 4, 5, 6, 7)
+
+
+#tests for adding array to array
+def test_add_array():
+    assert testArray.__add__(testArray2) == Array((4,), 3, 6, 9, 12)
+    assert testArray.__add__(failarray) == NotImplemented
+
+def test_add_2d():
+    assert test2d + test2d2 == Array((2, 3), 6, 10, -9, 10, 6, 11)
+    assert test2d + 5 == Array((2, 3), 8, 10, 6, 10, 11, 11)
+
+#tests for subtracting element from array
+def test_sub_element():
+    assert testArray.__sub__(5) == Array((4,), -4, -3, -2, -1)
+    assert testArray.__sub__(3) == Array((4,), -2, -1, 0, 1)
+
+#tests for subtracting array from array
+def test_sub_array():
+    assert testArray.__sub__(testArray2) == Array((4,), -1, -2, -3, -4)
+    assert testArray.__sub__(failarray) == NotImplemented
+
+def test_sub_2d():
+    assert test2d - test2d2 == Array((2, 3), 0, 0, 11, 0, 6, 1)
+    assert test2d - 5 == Array((2, 3), -2, 0, -4, 0, 1, 1)
+
+#tests for multiplying element with array
+def test_mul_element():
+    assert testArray.__mul__(5) == Array((4,), 5, 10, 15, 20)
+    assert testArray.__mul__(3) == Array((4,), 3, 6, 9, 12)
+
+#tests for multiplying array with array
+def test_mul_array():
+    assert testArray.__mul__(testArray2) == Array((4,), 2, 8, 18, 32)
+    assert testArray.__mul__(failarray) == NotImplemented
+
+def test_mul_2d():
+    assert test2d * test2d2 == Array((2, 3), 9, 25, -10, 25, 0, 30)
+    assert test2d * 5 == Array((2, 3), 15, 25, 5, 25, 30, 30)
+
+#test for checking if array is identical
+def test_eq():
+    assert testArray.__eq__(testArray2) == False
+    assert testArray.__eq__(failarray) == False
+    assert testArray.__eq__(testArray) == True
+
+def test_eq_2d():
+    assert test2d != test2d2
+    assert test2d == test2d
+
+#test for checking which elements are equal to a position in an array
+def test_is_equal():
+    assert testArray.is_equal(testArray2) == Array((4,), False, False, False, False)
+    assert testArray.is_equal(3) == Array((4,), False, False, True, False)
+
+def test_is_equal_2d():
+    assert test2d.is_equal(test2d2) == Array((2, 3), True, True, False, True, False, False)
+    assert test2d.is_equal(test2d) == Array((2, 3), True, True, True, True, True, True)
+
+#test for mean of an array
+def test_mean():
+    assert testArray.mean() == 2.5
+    assert testArray2.mean() == 5
+
+def test_mean_2d():
+    assert test2d.mean() == pytest.approx(4.333333)
+    assert test2d2.mean() == pytest.approx(1.333333)
+
+#tests for variance of an array
+def test_variance():
+    assert testArray.variance() == 1.25
+    assert testArray2.variance() == 5
+
+def test_variance_2d():
+    assert test2d.variance() == pytest.approx(3.222222)
+    assert test2d2.variance() == pytest.approx(28.888889)
+
+#tests for min_element of an array
+def test_min_element():
+    assert testArray.min_element() == 1
+    assert testArray2.min_element() != 5
+    assert failarray.min_element() == 3
+
+def test_min_element_2d():
+    assert test2d.min_element() == 1
+    assert test2d2.min_element() == -10
