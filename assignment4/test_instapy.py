01b6f4cdef47dbc77fc5eff491208b8342a77eee
Sigve <sigvetl@student.matnat.uio.no>
2020-10-02 12:01:47 +0200

    changed tests to hold for re-flipped image in color2sepia

--- assignment4/test_instapy.py
+++ assignment4/test_instapy.py
@@ -4,13 +4,15 @@ import numpy as np
 import random as r
 import cv2
 
+#creating a random pixel picture
 testarr = np.random.randint(0,255,(400, 600, 3))
-sepia_matrix = np.array([[0.393, 0.769, 0.189],
-                [0.349, 0.686, 0.168],
-                [0.272, 0.534, 0.131]])
 
+sepia_matrix = np.array([[0.393, 0.769, 0.189],
+                         [0.349, 0.686, 0.168],
+                         [0.272, 0.534, 0.131]])
 gray_matrix = np.array([0.07, 0.72, 0.21])
 
+#saving the random picture as testarr.jpg
 cv2.imwrite('testarr.jpg', testarr)
 image = cv2.imread('testarr.jpg')
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -30,6 +32,10 @@ def test_sepia_pixel():
     pixel[2] = image[255,590,:].dot(sepia_matrix[2,:])
     pixel = np.clip(pixel, 0, 255)
     pixel = pixel.astype("uint8")
+    #flipping the image back
+    temp = pixel[0]
+    pixel[0] = pixel[2]
+    pixel[2] = temp
     assert np.all(pixel == sepia_image[255,590,:])
     assert np.all(pixel == numba_sepia_image[255,590,:])
     assert np.all(pixel == python_sepia_image[255,590,:])
