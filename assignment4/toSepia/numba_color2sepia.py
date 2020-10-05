056cc6036e6d7bc6537e242e496f23169a071cfe
Tale <talehis@gmail.com>
2020-10-05 21:10:31 +0200

    added to docstrings and removed duplicate code

--- assignment4/toSepia/numba_color2sepia.py
+++ assignment4/toSepia/numba_color2sepia.py
@@ -13,14 +13,23 @@ matrise = np.array([[0.393, 0.769, 0.189],
                 [0.349, 0.686, 0.168],
                 [0.272, 0.534, 0.131]])
 
-"""
-creating a zero initialized array to hold the new values with the same size.
-looping through all indicies and assigning the cross product of red, green and blue with the average
-of the first, second and third row of the matrix. when k is initialized for i and j, checking for values above
-255 and setting to max-value if above. returning. casting and writing to file outside of function.
-"""
+
 @jit(nopython=True)
 def color2sepia(image, matrix):
+# A standard for docstrings is to include information about arguments and return values if there are any
+    """
+    creating a zero initialized array to hold the new values with the same size.
+    looping through all indicies and assigning the cross product of red, green and blue with the average
+    of the first, second and third row of the matrix. when k is initialized for i and j, checking for values above
+    255 and setting to max-value if above. returning. casting and writing to file outside of function.
+
+    Args:
+        image (numpy ndarray): a 3 dimensional array containing the pixel values of the colored image
+        matrix (numpy ndarray): a 2 dimensional array containing the sepia color weights
+
+    Returns:
+        img (numpy ndarray): a 3 dimensional array containing the sepia color pixel values as floats
+    """
     max = 255
     img = np.zeros(image.shape)
     for i in range(img.shape[0]):
@@ -34,29 +43,54 @@ def color2sepia(image, matrix):
                     img[i][j][k] = max
     return img
 
-time1_py = time.time()
-sepia_img = color2sepia(image, matrise)
-sepia_img = sepia_img.astype("uint8")
-sepia_img = cv2.cvtColor(sepia_img, cv2.COLOR_RGB2BGR)
-cv2.imwrite('rain_sepia_numba.jpeg', sepia_img)
-time2_py = time.time()
-exec = time2_py-time1_py
-print(exec)
 
-time1_py = time.time()
-sepia_img = color2sepia(image, matrise)
-sepia_img = sepia_img.astype("uint8")
-sepia_img = cv2.cvtColor(sepia_img, cv2.COLOR_RGB2BGR)
-cv2.imwrite('rain_sepia_numba.jpeg', sepia_img)
-time2_py = time.time()
-exec = time2_py-time1_py
-print(exec)
 
-time1_py = time.time()
-sepia_img = color2sepia(image, matrise)
-sepia_img = sepia_img.astype("uint8")
-sepia_img = cv2.cvtColor(sepia_img, cv2.COLOR_RGB2BGR)
-cv2.imwrite('rain_sepia_numba.jpeg', sepia_img)
-time2_py = time.time()
-exec = time2_py-time1_py
-print(exec)
+
+#To avoid duplicate code when timing the run time you can have a timing-function
+def runtime():
+    """
+    Times the runtime of the function python_color2sepia including the time it takes to cast the grayscale numpy array values
+    to int, and writing to file
+
+    Returns:
+        time2_num-time1_num (float): the time taken to run the function
+    """
+    time1_py = time.time()
+    sepia_img = color2sepia(image, matrise)
+    sepia_img = sepia_img.astype("uint8")
+    sepia_img = cv2.cvtColor(sepia_img, cv2.COLOR_RGB2BGR)
+    cv2.imwrite('rain_sepia_numba.jpeg', sepia_img)
+    time2_py = time.time()
+    return time2_py-time1_py
+
+print(runtime())
+print(runtime())
+print(runtime())
+
+
+# time1_py = time.time()
+# sepia_img = color2sepia(image, matrise)
+# sepia_img = sepia_img.astype("uint8")
+# sepia_img = cv2.cvtColor(sepia_img, cv2.COLOR_RGB2BGR)
+# cv2.imwrite('rain_sepia_numba.jpeg', sepia_img)
+# time2_py = time.time()
+# exec = time2_py-time1_py
+# print(exec)
+#
+# time1_py = time.time()
+# sepia_img = color2sepia(image, matrise)
+# sepia_img = sepia_img.astype("uint8")
+# sepia_img = cv2.cvtColor(sepia_img, cv2.COLOR_RGB2BGR)
+# cv2.imwrite('rain_sepia_numba.jpeg', sepia_img)
+# time2_py = time.time()
+# exec = time2_py-time1_py
+# print(exec)
+#
+# time1_py = time.time()
+# sepia_img = color2sepia(image, matrise)
+# sepia_img = sepia_img.astype("uint8")
+# sepia_img = cv2.cvtColor(sepia_img, cv2.COLOR_RGB2BGR)
+# cv2.imwrite('rain_sepia_numba.jpeg', sepia_img)
+# time2_py = time.time()
+# exec = time2_py-time1_py
+# print(exec)
