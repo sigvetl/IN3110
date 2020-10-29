f5d49f887c986e3d0e9335de304cd03a1d5ef00a
Sigve <sigvetl@student.matnat.uio.no>
2020-10-29 15:44:36 +0100

    Task 5.2

--- assignment5/filter_urls/filter_urls.py
+++ assignment5/filter_urls/filter_urls.py
@@ -1,11 +1,10 @@
 import re
 from requesting_urls.requesting_urls import get_html
 
-
 """
 Splitting a HTML documents into a list of URLs that are contained in the document
 
-Args: A HTML string
+Args: A HTML string and the base of the URL
 
 Returns: A list of all found URLs found in the HTML-string
 """
@@ -14,22 +13,25 @@ def find_urls(html_doc, baseurl):
     #capture a group that does not contain " or # before ending capture when coming to a " or #
     regex = "<a .*?href=\"([^\"#]+)[\"|#]"
     matches = re.findall(regex, html_doc)
-    print(len(matches))
-    list = []
-    #adding prefix to relative URLs
-    for e in matches:
-        if e[0] == '/' and e[1] == '/':
-            #hardcoded https
-            e = "https:" + e
-            list.append(e)
-        elif e[0] == '/' and e[1] != '/':
-            e = baseurl + e
-            list.append(e)
-        else:
-            list.append(e)
-    return list
+    protocol = re.findall("(https:|http:)", baseurl)[0]
+    for e in range(len(matches)):
+        r1 = (re.sub(r'(^\/[^\/].*)', baseurl + r'\g<1>', matches[e]))
+        r2 = (re.sub(r'(^\/\/.*)', protocol + r'\g<1>', matches[e]))
+        #can probably be intertwined using '((^\/[^\/].*)|(^\/\/.*))'
+        if (r1 != matches[e]):
+            matches[e] = r1
+        elif(r2 != matches[e]):
+            matches[e] = r2
+    return matches
 
-#does not filter links not starting with but containing cowikipedia.org
+"""
+Searches through a list of URLs and filters out the ones not matched by the regex
+
+Args: A HTML string, the base of the URL and the name of the file to write links to
+
+Returns: The list of Wikipedia-articles
+"""
+#does not filter links not starting with but containing wikipedia.org
 def find_articles(html_string, baseurl, name):
     urls = find_urls(html_string, baseurl)
     regex = "(https://.*\.wikipedia.org[^:]*$)"
@@ -37,22 +39,27 @@ def find_articles(html_string, baseurl, name):
     for links in urls:
         if re.findall(regex, links):
             matches.append(links)
-    #matches = [re.findall(regex, links) for links in list_of_links]
-    #matches = [match != [] for match in matches]
     write_to_file(matches, name)
     return matches
 
+"""
+Writes the list of links to the specified filename
+
+Args: A list of links and a filename
+
+Return: Nothing
+"""
 def write_to_file(list_of_links, name):
     f = open(name, "w")
     for link in list_of_links:
         f.write(link + "\n")
     f.close()
 
+"""
 nobel_prize = get_html("https://en.wikipedia.org/wiki/Nobel_Prize")
 bundesliga = get_html("https://en.wikipedia.org/wiki/Bundesliga")
 fis = get_html("https://en.wikipedia.org/wiki/2019%E2%80%9320_FIS_Alpine_Ski_ World_Cup")
 nobel_articles = find_articles(nobel_prize, "https://en.wikipedia.org", 'nobel_prize.txt')
 bundes_articles = find_articles(bundesliga, "https://en.wikipedia.org", 'bundesliga.txt')
 fis_articles = find_articles(fis, "https://en.wikipedia.org", 'fis.txt')
-#for e in a:
-#    print(e)
+"""
