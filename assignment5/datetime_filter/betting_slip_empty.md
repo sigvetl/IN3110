6d577a2ee2a6ecc2a14f81fb942e650281286be2
Sigve <sigvetl@student.matnat.uio.no>
2020-10-30 23:19:36 +0100

    Task 5.4

---
+++ assignment5/datetime_filter/betting_slip_empty.md
@@ -0,0 +1,43 @@
+
+BETTING SLIP
+
+Name:
+
+DATE | VENUE | DISCIPLINE | Who wins?
+--- | --- | --- | ---
+27/10/2019 | Sölden | Giant Slalom | 
+24/11/2019 | Levi | Slalom | 
+30/11/2019 | Lake Louise | Downhill | 
+01/12/2019 | Lake Louise | Super Giant Slalom | 
+06/12/2019 | Beaver Creek | Super Giant Slalom | 
+07/12/2019 | Beaver Creek | Downhill | 
+08/12/2019 | Beaver Creek | Giant Slalom | 
+15/12/2019 | Beaver Creek | Slalom | 
+20/12/2019 | Val Gardena/Gröden | Super Giant Slalom | 
+22/12/2019 | Alta Badia | Giant Slalom | 
+23/12/2019 | Alta Badia | Parallel Giant Slalom | 
+27/12/2019 | Bormio | Downhill | 
+28/12/2019 | Bormio | Downhill | 
+29/12/2019 | Bormio | Alpine Combined | 
+05/01/2020 | Zagreb | Slalom | 
+08/01/2020 | Madonna di Campiglio | Slalom | 
+11/01/2020 | Adelboden | Giant Slalom | 
+12/01/2020 | Adelboden | Slalom | 
+17/01/2020 | Wengen | Alpine Combined | 
+18/01/2020 | Wengen | Downhill | 
+19/01/2020 | Wengen | Slalom | 
+24/01/2020 | Kitzbühel | Super Giant Slalom | 
+25/01/2020 | Kitzbühel | Downhill | 
+26/01/2020 | Kitzbühel | Slalom | 
+28/01/2020 | Schladming | Slalom | 
+01/02/2020 | Garmisch-Partenkirchen | Downhill | 
+02/02/2020 | Garmisch-Partenkirchen | Giant Slalom | 
+08/02/2020 | Chamonix | Slalom | 
+09/02/2020 | Chamonix | Parallel Giant Slalom | 
+13/02/2020 | Saalbach-Hinterglemm | Downhill | 
+14/02/2020 | Saalbach-Hinterglemm | Super Giant Slalom | 
+22/02/2020 | Naeba | Giant Slalom | 
+29/02/2020 | Hinterstoder | Super Giant Slalom | 
+01/03/2020 | Hinterstoder | Alpine Combined | 
+02/03/2020 | Hinterstoder | Giant Slalom | 
+07/03/2020 | Kvitfjell | Downhill | 
---
+++ assignment5/datetime_filter/time_planner.py
@@ -0,0 +1,82 @@
+from bs4 import BeautifulSoup
+import re
+from dateutil import parser
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+from requesting_urls.requesting_urls import get_html
+
+"""
+Matches the event-keys with the event-values
+
+Args: A resultset containing the poption-tag
+
+Returns: A dict of discipline event keys and values
+"""
+def get_captions(captions):
+    count = 0
+    dict = {}
+    for e in captions[0]:
+        if count % 2 == 0:
+            key = re.findall("([A-Z]{2})", e)
+        elif count % 2 == 1:
+            value = re.findall(">(.*)<", str(e))
+            dict[key[0]] = value[0]
+        count +=1
+    return dict
+
+
+"""
+Fetches the table, indexes the list of events and writes all events to a file using markdown
+
+Args: A list containing lists of events
+
+Return: Nothing
+"""
+def extract_events(url):
+    html = get_html(url)
+    document = BeautifulSoup(html, "lxml")
+    table = document.find("table", {"class" : 'wikitable plainrowheaders'})
+    captions = table.find_all("caption")
+    disciplines = get_captions(captions)
+    #e = discipline.find_all("a")
+    rows = table.find_all("tr")
+    f = open("betting_slip_empty.md", "w")
+    f.write("\n" + "BETTING SLIP" + "\n\n")
+    f.write("Name:" + "\n\n")
+    f.write("DATE | VENUE | DISCIPLINE | Who wins?\n")
+    f.write("--- | --- | --- | ---\n")
+    slip = []
+    for row in rows[1:]:
+        cells = row.find_all(["td", "th"])
+        cells_text = [cell.get_text(strip=True) for cell in cells]
+        #filter non-events
+        if len(cells_text) > 5:
+            date = cells_text[2]
+            #if venue is empty keep previous
+            if len(cells_text) == 9:
+                venue = cells_text[3]
+                discipline = cells_text[4]
+            else:
+                discipline = cells_text[3]
+            slip.append([date, venue, discipline])
+    for row in slip:
+        date_regex = "(\d[^]].*\d)"
+        row[0] = re.findall(date_regex, row[0])[0]
+        row[0] = parser.parse(row[0])
+        discipline_regex = "([A-Z]{2})"
+        row[2] = re.findall(discipline_regex, row[2])[0]
+        #sub with dict
+        for key in disciplines:
+            if key == row[2]:
+                row[2] = disciplines[key]
+        f.write(row[0].strftime("%d/%m/%Y") + " | " + row[1] + " | " + row[2] + " | \n")
+    f.close()
+
+
+#table = format_table(url)
+url2020 = "https://en.wikipedia.org/wiki/2020%E2%80%9321_FIS_Alpine_Ski_World_Cup"
+url2019 = "https://en.wikipedia.org/wiki/2019%E2%80%9320_FIS_Alpine_Ski_World_Cup"
+extract_events(url2020)
+extract_events(url2019)
